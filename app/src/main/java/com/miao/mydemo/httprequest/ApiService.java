545e478435abd84bf7ebf1ee2cb1812888f0be18
miaozhuang <miao3316008@gamil.com>
2018-09-26 16:05:11 +0800

    add post example

--- app/src/main/java/com/miao/mydemo/httprequest/ApiService.java
+++ app/src/main/java/com/miao/mydemo/httprequest/ApiService.java
@@ -1,10 +1,14 @@
 package com.miao.mydemo.httprequest;
 
+import com.miao.mydemo.httprequest.model.BaseResponse;
 import com.miao.mydemo.httprequest.model.Test1;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
@@ -24,5 +28,18 @@ public class ApiService {
     public interface HttpTestService2 {
         @GET("article/list/{page}/json")
         Call<ResponseBody> getJSON(@Path("page") int page);
+    }
+
+    //POST有参数示例
+    public interface HttpTestService3 {
+        @FormUrlEncoded
+        @POST("HomePage/CallSend")
+        Call<BaseResponse> call(
+                @Field("phone") String phone,
+                @Field("loginName") String loginName,
+                @Field("timestamp") String timestamp,
+                @Field("sign") String sign,
+                @Field("versionCode") String versionCode,
+                @Field("deviceType") String deviceType);
     }
 }
